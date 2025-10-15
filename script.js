c52d81fddada4eab614e8453c340e19bec36133b
Cachone <cachonefabio@gmail.com>
2025-10-15 00:51:18 -0300

    Fix chapter title typo and implement navigation functionality for next/previous chapters

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const botaoPlayPause = document.getElementById("play-pause");
+const botaoProximoCapitulo = document.getElementById("proximo");
+const botaoCapituloAnterior = document.getElementById("anterior");
 const audio = document.getElementById("audio-capitulo");
+const textoCapitulo = document.getElementById("capitulo");
+const totalCapitulos = 10;
 
 let taTocando = false;
+let capituloAtual = 1;
 
 function tocarFaixa() {
   audio.play();
   taTocando = true;
+
+  botaoPlayPause.classList.add("tocando");
 }
 
 function pausarFaixa() {
   audio.pause();
   taTocando = false;
+
+  botaoPlayPause.classList.remove("tocando");
 }
 
 function tocarOuPausa() {
@@ -21,4 +30,34 @@ function tocarOuPausa() {
   }
 }
 
+function proximoCapitulo() {
+  pausarFaixa();
+
+  if (capituloAtual < totalCapitulos) {
+    capituloAtual++;
+  } else {
+    capituloAtual = 1;
+  }
+
+  audio.src = "./audios/" + capituloAtual + ".mp3";
+  textoCapitulo.innerText = "Capítulo " + capituloAtual;
+  tocarFaixa();
+}
+
+function capituloAnterior() {
+  pausarFaixa();
+
+  if (capituloAtual === 1) {
+    capituloAtual = totalCapitulos;
+  } else {
+    capituloAtual--;
+  }
+
+  audio.src = "./audios/" + capituloAtual + ".mp3";
+  textoCapitulo.innerText = "Capítulo " + capituloAtual;
+  tocarFaixa();
+}
+
 botaoPlayPause.addEventListener("click", tocarOuPausa);
+botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
+botaoCapituloAnterior.addEventListener("click", capituloAnterior);
